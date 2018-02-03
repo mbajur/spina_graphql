e34afa03976b06e1431ad26f4ef935374b405fc6
Michal Bajur <mbajur@gmail.com>
2018-02-03 11:06:45 +0100

    Fix

--- app/graphql/spina_graphql/types/photo_collection_type.rb
+++ app/graphql/spina_graphql/types/photo_collection_type.rb
@@ -3,11 +3,5 @@ SpinaGraphql::Types::PhotoCollectionType = GraphQL::ObjectType.define do
 
   field :id, types.ID
 
-  # field :value, types.String do
-  #   resolve ->(obj, args, ctx) do
-  #     obj.try(:html_safe)
-  #   end
-  # end
-
   connection :photos, SpinaGraphql::Types::PhotoType.connection_type
 end
