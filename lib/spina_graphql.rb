793d221688e2f3851dd5ee1423abb0c745720938
Michal Bajur <mbajur@gmail.com>
2018-02-07 12:29:42 +0100

    Testfix

--- lib/spina_graphql.rb
+++ lib/spina_graphql.rb
@@ -1,8 +1,12 @@
-require 'spina_graphql/engine'
+require 'spina'
+require 'mobility'
 require 'graphql'
 require 'graphql/guard'
 require 'pg'
+require 'spina_graphql/engine'
 
 module SpinaGraphql
   # Your code goes here...
 end
+
+require_relative 'spina_graphql/services'
--- lib/spina_graphql/engine.rb
+++ lib/spina_graphql/engine.rb
@@ -1,9 +1,3 @@
-require 'spina'
-require 'pg'
-require 'mobility'
-
-require_relative 'services'
-
 module SpinaGraphql
   class Engine < ::Rails::Engine
     isolate_namespace SpinaGraphql
